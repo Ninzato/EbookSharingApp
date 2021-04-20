e77fdae4bfffb76f8aad26a691032084e0626293
Ninzato <61687110+Ninzato@users.noreply.github.com>
2021-04-21 03:25:00 +0800

    Add files via upload

---
+++ storage/framework/views/b3e5d0658108cfa267733e9893e182f6d6876e15.php
@@ -0,0 +1,16 @@
+
+
+<?php $__env->startSection('title', 'About us'); ?>
+
+<?php $__env->startSection('content'); ?>
+
+    <div class="heading">
+        <h1>This is E-Book sharing app</h1>
+    </div>
+    <div class="sub-heading">
+        <h2>This app was made for class exercise</h2>
+    </div>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('subview.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\ebookSharingApp\resources\views/subview/about.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f48860a1de98e1e31af9e9d6acc8a81904ce1233.php
@@ -0,0 +1,29 @@
+
+
+<?php $__env->startSection('title', 'List of Books'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <div class="heading">
+        <h1>Here are the books that we have</h1>
+    </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>ISBN</th>
+                    <th>Category</th>
+                    <th>Pages</th>
+                </tr>
+            </thead>
+            <?php $__currentLoopData = $list; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $book): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <tr>
+                    <?php $__currentLoopData = $book; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $data): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <td><?php echo e($data); ?></td>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </tr>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </table>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('subview.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\ebookSharingApp\resources\views/subview/list.blade.php ENDPATH**/ ?>
